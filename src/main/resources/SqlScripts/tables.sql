57cd4bef3baf038d94972d46a663388845f1843f
Enoc Santiago <enocsanti2@gmail.com>
2020-04-02 11:08:06 -0400

    initial sql script write-up

--- src/main/resources/SqlScripts/tables.sql
+++ src/main/resources/SqlScripts/tables.sql
@@ -1,22 +1,27 @@
-drop if exists superhumans;
-create table superhumans(
-	id serial primary key,
+drop if exists superhuman;
+create table superhuman(
+	hero_id serial primary key,
 	firstName varchar,
 	lastName varchar,
 	alias,
-	originStory text
+	originStory text,
+	align_id foreign key alignment(align_id),
+	team_id foreign key super_team(team_id)
 );
 
 
 -- Enumeration Table
 drop if exists alignment;
 create table alignment(
+	align_id numeric primary key;
+	kind varchar;
 
 );
 
 
--- Junction Table
 drop if exists super_team;
 create table super_team(
-	
+	team_id serial primary key,
+	team_name varchar,
+	hero_id numeric foreign key superhuman(hero_id)
 );
